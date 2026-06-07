28e0382b74736e567b7e45f75f3079dbdb37b007
Elvis Alam <88417025+theelvisalam@users.noreply.github.com>
2026-06-07 17:35:32 -0500

    stacks

---
+++ Stacks/20_Valid_Parenthesis.py
@@ -0,0 +1,32 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+'''
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        valid = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{',
+        }
+
+        for p in s:
+            if p in valid:
+                if stack and stack[-1] == valid[p]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(p)
+
+        return True if not stack else False
