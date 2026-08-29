36e424c3ea40ec0383e9d13c83b4464efd39bf3a
Elvis Alam <alamelvism@gmail.com>
2026-08-29 13:42:49 -0500

    incomplete

---
+++ HashMap/valid-sudoku.py
@@ -0,0 +1,33 @@
+'''
+You are given a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+    Each row must contain the digits 1-9 without duplicates.
+    Each column must contain the digits 1-9 without duplicates.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+
+Return true if the Sudoku board is valid, otherwise return false
+
+Note: A board does not need to be full or be solvable to be valid.
+'''
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                val =  board[row][col]
+                print(val)
+
+
+board = [
+    ["1","2",".",".","3",".",".",".","."],
+    ["4",".",".","5",".",".",".",".","."],
+    [".","9","8",".",".",".",".",".","3"],
+    ["5",".",".",".","6",".",".",".","4"],
+    [".",".",".","8",".","3",".",".","5"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".",".",".",".",".",".","2",".","."],
+    [".",".",".","4","1","9",".",".","8"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+sol = Solution()
+print(sol.isValidSudoku(board))
