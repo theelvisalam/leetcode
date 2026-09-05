c8b759d92ec186d03e672c43342e65f4e82548b4
Elvis Alam <alamelvism@gmail.com>
2026-09-04 21:55:41 -0500

    incomplete

---
+++ sliding-window/longest-substring-without-repeating-characters.py
@@ -0,0 +1,26 @@
+'''
+Given a string s, find the length of the longest substring without duplicate characters.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: s = "zxyzxyz"
+
+Output: 3
+
+Explanation: The string "xyz" is the longest without duplicate characters.
+
+Example 2:
+
+Input: s = "xxxx"
+
+Output: 1
+'''
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+
+s = "zxyzxyz"
+sol = Solution()
+print(sol.lengthOfLongestSubstring(s))
