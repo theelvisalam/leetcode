7e0240824046e9f3ba47978380a3fee5fadaac90
Elvis Alam <alamelvism@gmail.com>
2026-09-18 20:24:11 -0500

    completed

--- HashMap/valid-anagram.py
+++ HashMap/valid-anagram.py
@@ -23,41 +23,27 @@ Input: s = "x", t = "x"
 
 Output: true
 '''
+from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if s is None or t is None:
-            return False
         if len(s) != len(t):
             return False
 
-        ls = {}
-        lt = {}
+        lt = defaultdict(int)
+        ls = defaultdict(int)
+        
+        for l in t:
+            lt[l] += 1
 
         for l in s:
-            if l not in ls:
-                ls[l] = 1
-            else:
-                ls[l] = ls.get(l) + 1
+            ls[l] += 1
 
-        for l in t:
-            if l not in lt:
-                lt[l] = 1
-            else:
-                lt[l] = lt.get(l) + 1
-
-        for l, ct in ls.items():
-            if l not in lt.keys():
-                return False
-            if lt[l] == ct:
-                continue
-            else:
-                return False
-
+        for l, i in lt.items():
+            if ls[l] != i:
+               return False
         return True
 
-
-
-s = "racreac"
+s = "racecar"
 t = "carrace"
 sol = Solution()
 print(sol.isAnagram(s, t))
