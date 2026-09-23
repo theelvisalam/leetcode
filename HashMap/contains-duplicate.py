836ed537d70ae18c653e0ae2b8df4fe1154a0abb
Elvis Alam <alamelvism@gmail.com>
2026-09-23 09:40:15 -0500

    incomplete

--- HashMap/contains-duplicate.py
+++ HashMap/contains-duplicate.py
@@ -17,13 +17,6 @@ Output: false
 from collections import defaultdict
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
 
 input = [1, 2, 1, 4]
 sol = Solution()
