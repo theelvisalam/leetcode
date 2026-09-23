255b622a34924d7d69b5ae2747acefbc726ded81
Elvis Alam <alamelvism@gmail.com>
2026-09-22 20:42:48 -0500

    incomplete

---
+++ HashMap/contains-duplicate-ii.py
@@ -0,0 +1,28 @@
+'''
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+'''
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+
+k = 2
+nums = [1,2,3,1,2,3]
+sol = Solution()
+print(sol.containsNearbyDuplicate(nums))
+        
