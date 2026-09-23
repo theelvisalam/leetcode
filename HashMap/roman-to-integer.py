31a24eb939525ee91d448d3df707d9ce65829885
Elvis Alam <alamelvism@gmail.com>
2026-09-22 20:18:38 -0500

    completed

--- HashMap/roman-to-integer.py
+++ HashMap/roman-to-integer.py
@@ -37,7 +37,30 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        rn = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+        res = 0
+
+        for i in range(len(s)):
+            curr = rn[s[i]]
+            if i + 1 < len(s):
+                next = rn[s[i + 1]]
+            else:
+                next = 0
+            if curr < next:
+                res -= curr
+            else:
+                res += curr
+
+        return res
+
+s = "MCMXCIV"
+sol = Solution()
+print(sol.romanToInt(s))
