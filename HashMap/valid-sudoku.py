6b963087ee728431e16ac671fc5fa95e80ea302c
Elvis Alam <alamelvism@gmail.com>
2026-09-24 21:47:41 -0500

    completed

--- HashMap/valid-sudoku.py
+++ HashMap/valid-sudoku.py
@@ -8,29 +8,63 @@ You are given a 9 x 9 Sudoku board board. A Sudoku board is valid if the followi
 Return true if the Sudoku board is valid, otherwise return false
 
 Note: A board does not need to be full or be solvable to be valid.
+
+Input: board =
+[["1","2",".",".","3",".",".",".","."],
+ ["4",".",".","5",".",".",".",".","."],
+ [".","9","8",".",".",".",".",".","3"],
+ ["5",".",".",".","6",".",".",".","4"],
+ [".",".",".","8",".","3",".",".","5"],
+ ["7",".",".",".","2",".",".",".","6"],
+ [".",".",".",".",".",".","2",".","."],
+ [".",".",".","4","1","9",".",".","8"],
+ [".",".",".",".","8",".",".","7","9"]]
+
+Output: true
+
+Example 2:
+
+Input: board =
+[["1","2",".",".","3",".",".",".","."],
+ ["4",".",".","5",".",".",".",".","."],
+ [".","9","1",".",".",".",".",".","3"],
+ ["5",".",".",".","6",".",".",".","4"],
+ [".",".",".","8",".","3",".",".","5"],
+ ["7",".",".",".","2",".",".",".","6"],
+ [".",".",".",".",".",".","2",".","."],
+ [".",".",".","4","1","9",".",".","8"],
+ [".",".",".",".","8",".",".","7","9"]]
+
+Output: false
+
+Explanation: There are two 1's in the top-left 3x3 sub-box.
 '''
+from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        rows = {}
-        cols = {}
-        subs = {}
-
-        for row in range(9):
-            d=
-            for col in range(9):
-                rows[row] = board[row]
-                cols[col] = board[col]
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == ".":
+                    continue
+                if (board[row][col] in cols or board[row][col] in rows or board[row][col] in squares[(row // 3, col // 3)]):
+                    return False
+                cols[col].add(board[row][col])
+                rows[row].add(board[row][col])
+                squares[(row // 3, col // 3)].add(board[row][col])
         print(rows)
-        pr:
 
+        return True
 
 board = [
     ["1","2",".",".","3",".",".",".","."],
     ["4",".",".","5",".",".",".",".","."],
     [".","9","8",".",".",".",".",".","3"],
-    ["5",".",".",".","6",".",".",".","4"
-    [".",".",".","8",".","3",".",".","jk5"],
+    ["5",".",".",".","6",".",".",".","4"],
+    [".",".",".","8",".","3",".",".","5"],
     ["7",".",".",".","2",".",".",".","6"],
     [".",".",".",".",".",".","2",".","."],
     [".",".",".","4","1","9",".",".","8"],
